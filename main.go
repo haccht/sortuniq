eacb3ccd3ac319dffc100a1083cc3931915039e7
haccht <haccht@users.noreply.github.com>
2023-11-08 12:40:59 +0000

    Add text/tabwriter

--- main.go
+++ main.go
@@ -4,34 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
 	"os"
+	"slices"
 	"sort"
+	"text/tabwriter"
 
 	flags "github.com/jessevdk/go-flags"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
 type options struct {
-	Count   bool `short:"c" long:"count" description:"Prefix lines by the number of occurrences"`
-	Reverse bool `short:"r" long:"reverse" description:"Reverse the result"`
+	Count   bool `short:"c" long:"count" description:"Prefix the number of occurrences"`
+	Sort    bool `short:"n" long:"order" description:"Sort by the number of occurrences"`
+	Reverse bool `short:"r" long:"reverse" description:"Reverse the order"`
 }
 
-func main() {
+func run() error {
 	var opts options
 	args, err := flags.Parse(&opts)
 	if err != nil {
 		if fe, ok := err.(*flags.Error); ok && fe.Type == flags.ErrHelp {
 			os.Exit(0)
 		}
-		log.Fatal(err)
+		return err
 	}
 
 	readers := make([]io.Reader, 0, len(args)+1)
 	for _, arg := range args {
 		f, err := os.Open(arg)
 		if err != nil {
-			log.Fatalf("failed to open file %s: %s", arg, err)
+			return fmt.Errorf("failed to open file %s: %s", arg, err)
 		}
 		defer f.Close()
 		readers = append(readers, f)
@@ -49,7 +51,7 @@ func main() {
 		hash[scanner.Text()]++
 	}
 	if scanner.Err() != nil {
-		log.Fatalln(scanner.Err())
+		return scanner.Err()
 	}
 
 	keys := make([]string, 0, len(hash))
@@ -57,29 +59,37 @@ func main() {
 		keys = append(keys, k)
 	}
 
-	if opts.Count {
+	if opts.Sort {
 		sort.Slice(keys, func(i, j int) bool {
-			if opts.Reverse {
-				return hash[keys[i]] < hash[keys[j]]
-			} else {
-				return hash[keys[i]] > hash[keys[j]]
-			}
+			return hash[keys[i]] > hash[keys[j]]
 		})
-
-		for _, k := range keys {
-			fmt.Printf("%8d\t%s\n", hash[k], k)
-		}
 	} else {
 		sort.Slice(keys, func(i, j int) bool {
-			if opts.Reverse {
-				return keys[i] > keys[j]
-			} else {
-				return keys[i] < keys[j]
-			}
+			return keys[i] < keys[j]
 		})
+	}
+	if opts.Reverse {
+		slices.Reverse(keys)
+	}
 
+	if opts.Count {
+		w := tabwriter.NewWriter(os.Stdout, 4, 0, 1, ' ', tabwriter.AlignRight)
 		for _, k := range keys {
-			fmt.Printf("%s\n", k)
+			fmt.Fprintf(w, "%d\t  %s\n", hash[k], k)
 		}
+		w.Flush()
+	} else {
+		for _, k := range keys {
+			fmt.Println(k)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 }
